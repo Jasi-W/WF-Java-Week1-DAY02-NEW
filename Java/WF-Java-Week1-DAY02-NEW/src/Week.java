8ce038317f935f12449f27bb3ba86e29bc8b420e
Jasmine Wagner <jasminewagner@hotmail.com>
2019-11-19 14:08:19 +0100

    new zahlen und week

---
+++ Java/WF-Java-Week1-DAY02-NEW/src/Week.java
@@ -0,0 +1,25 @@
+public class Week {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        while (!(scan.hasNextInt())) {
+            System.out.println("Plese enter a number!");
+            System.out.print("Number: ");
+            scan.next();
+        }
+        int number = scan.nextInt();
+
+        String[] weekdays = { "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag" };
+
+
+        int i = 0;
+
+        if(number == ) {
+
+        }
+    }
+
+}
---
+++ Java/WF-Java-Week1-DAY02-NEW/src/Zahlen.java
@@ -0,0 +1,27 @@
+public class Zahlen {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        while (!(scan.hasNextInt())) {
+            System.out.println("Plese enter a number!");
+            System.out.print("Number: ");
+            scan.next();
+        }
+        int number = scan.nextInt();
+
+        if(number > 0){
+            System.out.print(number+ " is positiv ");
+        } else {
+            System.out.print(number+ " is negativ ");
+        }
+
+        if(number > 100) {
+            System.out.println("and large number");
+        } else if(number < 1) {
+            System.out.println("and small number");
+        }
+    }
+
+}
